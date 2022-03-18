b927828704f3aff748aa838e903aff498c2dacfa
L-j-h-c <ckrgkswnsgh@naver.com>
2022-03-18 23:48:49 +0900

    [Feat] 입출력문 기본 사용
    입출력문 기본 사용

--- ComputerProgramming/assignment/lab/src/Lab2_1.java
+++ ComputerProgramming/assignment/lab/src/Lab2_1.java
@@ -2,6 +2,22 @@ import java.util.Scanner;
 
 public class Lab2_1 {
     public static void main(String[] args) {
+        // 몇 개의 인풋을 받을지 입력
         Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        System.out.println(num);
+
+        // 위에서 입력한 수 만큼 인풋을 넣어줌
+        String[] arr = new String[num];
+        for (int i=0; i<num; i++) {
+            String in = scanner.next();
+            arr[i] = in;
+        }
+
+        // 인풋을 차례대로 프린트함
+        for (String elt : arr) {
+            System.out.print(elt + " ");
+        }
+        System.out.println();
     }
 }
