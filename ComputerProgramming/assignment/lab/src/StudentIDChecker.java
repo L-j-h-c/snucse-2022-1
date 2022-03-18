7a87c3d4a813d0ec0275db7565afdf4d5f533cbc
L-j-h-c <ckrgkswnsgh@naver.com>
2022-03-19 04:13:51 +0900

    [Feat] StudentIDChecker 과제 완성
    StudentIDChecker 과제 완성

--- ComputerProgramming/assignment/lab/src/StudentIDChecker.java
+++ ComputerProgramming/assignment/lab/src/StudentIDChecker.java
@@ -1,32 +1,51 @@
 import java.util.Scanner;
 
 public class StudentIDChecker {
+    static boolean isProperLength(String id) {
+        if (id.length() != 10) {
+            System.out.println("The input length should be 10.");
+            return false;
+        }
+        return true;
+    }
+
+    static boolean hasProperDivision(String id) {
+        if (id.charAt(4) != '-') {
+            System.out.println("Fifth character should be '-'.");
+            return false;
+        }
+        else
+            return true;
+    }
+
+    static boolean hasProperDigits(String id) {
+        for (int i = 0; i < id.length(); i++) {
+            if (i == 4)
+                continue;
+            if (id.charAt(i) < '0' || id.charAt(i) > '9') {
+                System.out.println("Contains an invalid digit.");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static void validateStudentID(String id) {
+        if(isProperLength(id) && hasProperDivision(id) && hasProperDigits(id)) {
+            System.out.println(id + "is a valid student ID.");
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String id = scanner.next();
-
-        if (id.length() != 10) {
-            System.out.println("The input length should be 10.");
-            return;
-        }
-        else if (id.charAt(4) != '-') {
-            System.out.println("Fifth character should be '-'.");
-            return;
-        }
-        else {
-            for (int i=0; i < id.length(); i++) {
-                if (i==4)
-                    continue;
-                if (id.charAt(i) < '0' || id.charAt(i) > '9') {
-                    System.out.println("Contains an invalid digit.");
-                    return;
-                }
+        do {
+            String id = scanner.next();
+            if(id.equals("exit")) {
+                break;
             }
-        }
-
-        // valid id
-        System.out.println(id + " is a valid student ID.");
+            validateStudentID(id);
+        } while(true);
     }
 }
 
