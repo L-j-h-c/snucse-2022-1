5f8e021f2fee59490d94aa6e11c8bb293a3786f1
L-j-h-c <ckrgkswnsgh@naver.com>
2022-03-19 17:08:18 +0900

    [PS] Draw Figure diamond shape
    Draw Figure diamond shape

--- ComputerProgramming/HomeWork/Solution/HW1/problem3/src/DrawingFigure.java
+++ ComputerProgramming/HomeWork/Solution/HW1/problem3/src/DrawingFigure.java
@@ -1,6 +1,39 @@
 public class DrawingFigure {
     public static void drawFigure(int n) {
-        // DO NOT change the skeleton code.
-        // You can add codes anywhere you want.
+        for(int i=1; i<=2*n-1; i++) {
+            for(int j=i+1; j<=n; j++) {
+                System.out.print("  ");
+            }
+            for(int j=n; j<i; j++) {
+                System.out.print("  ");
+            }
+
+            if(i<=n) {
+                for (int j = 1; j <= 2 * i - 1; j++) {
+                    if (j <= i) {
+                        System.out.print(j%10);
+                    } else {
+                        System.out.print((2*i-j)%10);
+                    }
+                    if(j!=2*i-1) System.out.print(" ");
+                }
+            } else {
+                for (int j = 1; j <= 2*(2*n-i)-1; j++) {
+                    if (j <= 2*n-i) {
+                        System.out.print(j%10);
+                    } else {
+                        System.out.print((2*(2*n-i)-j)%10);
+                    }
+                    if(j!=2*(2*n-i)-1) System.out.print(" ");
+                }
+            }
+            for(int j=i+1; j<=n; j++) {
+                System.out.print("  ");
+            }
+            for(int j=n; j<i; j++) {
+                System.out.print("  ");
+            }
+            if(i!=2*n-1) System.out.println();
+        }
     }
 }
