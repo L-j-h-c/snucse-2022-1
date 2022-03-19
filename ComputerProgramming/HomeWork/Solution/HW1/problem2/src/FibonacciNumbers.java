12731d59b1a4e2971558bc154fe5b7b3f0202178
L-j-h-c <ckrgkswnsgh@naver.com>
2022-03-19 17:07:55 +0900

    [PS] Fibonacci Sum
    Fibonacci Sum

--- ComputerProgramming/HomeWork/Solution/HW1/problem2/src/FibonacciNumbers.java
+++ ComputerProgramming/HomeWork/Solution/HW1/problem2/src/FibonacciNumbers.java
@@ -1,6 +1,28 @@
 public class FibonacciNumbers {
     public static void printFibonacciNumbers(int n) {
-        // DO NOT change the skeleton code.
-        // You can add codes anywhere you want.
+        int lastNum = 0;
+        int prevNum = 0;
+        int sum = 0;
+        for(int i=0; i<n; i++) {
+            System.out.print(lastNum + " ");
+            if (i == 0) {
+                lastNum = 1;
+            } else {
+                int tmp = lastNum;
+                lastNum += prevNum;
+                prevNum = tmp;
+            }
+            sum += lastNum;
+        }
+        sum -= lastNum;
+        System.out.println();
+        System.out.printf("sum : %s",sumToString(sum));
+    }
+
+    static String sumToString(int sum) {
+        if ((sum >= 100000) && ((sum%100000)/10000==0)) {
+            return "0" + Integer.toString(sum % 100000);
+        }
+        return Integer.toString(sum%100000);
     }
 }
