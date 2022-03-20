f0523a6f9db078fa1c7a82c29ded5937f9d21f9f
L-j-h-c <ckrgkswnsgh@naver.com>
2022-03-21 03:01:42 +0900

    [PS] Decreasing String
    Decreasing String

--- ComputerProgramming/HomeWork/Solution/HW1/problem7/src/DecreasingString.java
+++ ComputerProgramming/HomeWork/Solution/HW1/problem7/src/DecreasingString.java
@@ -1,6 +1,18 @@
 public class DecreasingString {
     public static void printLongestDecreasingSubstringLength(String inputString) {
-        // DO NOT change the skeleton code.
-        // You can add codes anywhere you want.
+        int length = inputString.length();
+        int tmpCount = 1;
+        int finalCount = 1;
+        for(int i=0; i<length-1; i++) {
+            if(inputString.charAt(i)>inputString.charAt(i+1)) {
+                tmpCount++;
+                if (finalCount<tmpCount) {
+                    finalCount = tmpCount;
+                }
+            } else {
+                tmpCount = 1;
+            }
+        }
+        System.out.println(finalCount);
     }
 }
