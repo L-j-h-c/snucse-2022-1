04a673f1811e5b134492c3a04f83d010e652971e
L-j-h-c <ckrgkswnsgh@naver.com>
2022-03-20 22:47:16 +0900

    [PS] CharacterPatternSearching
     CharacterPatternSearching

--- ComputerProgramming/HomeWork/Solution/HW1/problem4/src/CharacterPattern.java
+++ ComputerProgramming/HomeWork/Solution/HW1/problem4/src/CharacterPattern.java
@@ -1,6 +1,26 @@
 public class CharacterPattern {
     public static void searchCharPattern(String str) {
-        // DO NOT change the skeleton code.
-        // You can add codes anywhere you want.
+        for(int i=0; i<=str.length()-3; i++) {
+            if(consequtivenessChecker(str, i)!=' ') {
+                System.out.print(consequtivenessChecker(str, i));
+            }
+            if(middleChecker(str, i)!=' ') {
+                System.out.print(middleChecker(str, i));
+            }
+        }
+    }
+
+    static char consequtivenessChecker(String str, int i) {
+        if (str.charAt(i)+1==str.charAt(i+1) && str.charAt(i+1)+1==str.charAt(i+2)) {
+            return str.charAt(i+1);
+        }
+        return ' ';
+    }
+
+    static char middleChecker(String str, int i) {
+        if (str.charAt(i)==str.charAt(i+2) && str.charAt(i)-32==str.charAt(i+1)) {
+            return str.charAt(i+1);
+        }
+        return ' ';
     }
 }
