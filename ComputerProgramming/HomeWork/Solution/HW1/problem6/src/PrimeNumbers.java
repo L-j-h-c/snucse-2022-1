e7eb3e4cb593b37f4919581db89c3f18514249ff
L-j-h-c <ckrgkswnsgh@naver.com>
2022-03-21 02:33:04 +0900

    [PS] Prime Number
    Prime Number

--- ComputerProgramming/HomeWork/Solution/HW1/problem6/src/PrimeNumbers.java
+++ ComputerProgramming/HomeWork/Solution/HW1/problem6/src/PrimeNumbers.java
@@ -1,6 +1,21 @@
 public class PrimeNumbers {
     public static void printPrimeNumbers(int m, int n) {
-        // DO NOT change the skeleton code.
-        // You can add codes anywhere you want.
+        for(int i=m; i<=n; i++) {
+            int tmp = 2000;
+            if(i==2) {
+                System.out.print(i + " ");
+            }
+            for(int j=2; j<i; j++) {
+                if(j==i-1) {
+                    tmp = i;
+                }
+                if (i%j==0) {
+                    break;
+                }
+            }
+            if(tmp!=2000) {
+                System.out.print(i + " ");
+            }
+        }
     }
 }
