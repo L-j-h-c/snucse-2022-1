7391a616885f463603b878ead5f0e847a56d13d3
L-j-h-c <ckrgkswnsgh@naver.com>
2022-03-22 20:11:36 +0900

    [PS] #CP - HW1 - FractionNumberCalculator - 프로그램 완성
    #CP - HW1 - FractionNumberCalculator - 프로그램 완성

--- ComputerProgramming/HomeWork/Solution/HW1/problem9/src/FractionalNumberCalculator.java
+++ ComputerProgramming/HomeWork/Solution/HW1/problem9/src/FractionalNumberCalculator.java
@@ -1,7 +1,80 @@
 public class FractionalNumberCalculator {
+	private static char operator = ' ';
+	private static String firstFraction;
+	private static String secondFraction;
+	private static int numerator;
+	private static int denominator;
+
 	public static void printCalculationResult(String equation) {
-		// DO NOT change the skeleton code.
-		// You can add codes anywhere you want.
+		splitEquation(equation);
+		FractionalNumber fn = new FractionalNumber();
+		fn.gcd(numerator, denominator);
+		System.out.println(fn.getFraction());
+	}
+
+	// equation을 받아서 연산자, num1, num2로 나눠주는 메서드
+	private static void splitEquation(String equation) {
+		for(int i=0; i<=30 ;i++) {
+			if(equation.charAt(i)==' ') {
+				firstFraction = equation.substring(0,i);
+				operator = equation.charAt(i + 1);
+				secondFraction = equation.substring(i + 3);
+				break;
+			}
+		}
+		splitEachNumber();
+	}
+
+	// 각각의 분수들을 '/'를 전후로 분리
+	private static void splitEachNumber() {
+		int firstNumerator = 1;
+		int firstDenominator = 1;
+		int secondNumerator = 1;
+		int secondDenominator = 1;
+
+		for(int i=0; i<firstFraction.length() ;i++) {
+			if(firstFraction.charAt(i)=='/') {
+				firstNumerator = Integer.parseInt(firstFraction.substring(0,i));
+				firstDenominator = Integer.parseInt(firstFraction.substring(i+1));
+				break;
+			} else {
+				if(i==firstFraction.length()-1) firstNumerator = Integer.parseInt(firstFraction);
+			}
+		}
+		for(int i=0; i<secondFraction.length() ;i++) {
+			if(secondFraction.charAt(i)=='/') {
+				secondNumerator = Integer.parseInt(secondFraction.substring(0,i));
+				secondDenominator = Integer.parseInt(secondFraction.substring(i+1));
+				break;
+			} else {
+				if(i==secondFraction.length()-1) secondNumerator = Integer.parseInt(secondFraction);
+			}
+		}
+
+		calculate(firstNumerator, firstDenominator, secondNumerator, secondDenominator);
+	}
+
+	private static void calculate(int fn, int fd, int sn, int sd) {
+		switch (operator) {
+			case '+':
+				numerator = fn*sd+sn*fd;
+				denominator = fd*sd;
+				break;
+			case '-':
+				numerator = fn*sd-sn*fd;
+				denominator = fd*sd;
+				break;
+			case '*':
+				numerator = fn*sn;
+				denominator = fd*sd;
+				break;
+			case '/':
+				numerator = fn*sd;
+				denominator = fd*sn;
+				break;
+			default:
+				System.out.println("error");
+		}
 	}
 }
 
