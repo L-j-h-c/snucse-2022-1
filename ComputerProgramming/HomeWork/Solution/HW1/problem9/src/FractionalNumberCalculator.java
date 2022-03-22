8682fe3674a22ee72e29025a4c1506cd22dfb3fe
L-j-h-c <ckrgkswnsgh@naver.com>
2022-03-22 20:11:14 +0900

    [PS] #CP - HW1 - FractionNumberCalculator - GCD 계산
    #CP - HW1 - FractionNumberCalculator - GCD 계산

--- ComputerProgramming/HomeWork/Solution/HW1/problem9/src/FractionalNumberCalculator.java
+++ ComputerProgramming/HomeWork/Solution/HW1/problem9/src/FractionalNumberCalculator.java
@@ -9,4 +9,25 @@ class FractionalNumber {
 	private int numerator;
 	private int denominator;
 
+	// 최대공약수로 나눠주는 메서드
+	public void gcd(int n, int d) {
+		numerator = n;
+		denominator = d;
+		int gcd = 1;
+		for(int i=1; i<=d; i++) {
+			if(n%i==0 && d%i==0) {
+				gcd = i;
+			}
+		}
+		numerator /= gcd;
+		denominator /= gcd;
+	}
+
+	// 계산된 분모와 분자를 return하는 메서드
+	public String getFraction() {
+		if (this.denominator==1) return Integer.toString(this.numerator);
+		else {
+			return (Integer.toString(this.numerator)+"/"+Integer.toString(this.denominator));
+		}
+	}
 }
