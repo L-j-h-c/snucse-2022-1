95dab875d012ed61303e15a947480fd0af0de720
L-j-h-c <ckrgkswnsgh@naver.com>
2022-03-25 23:49:23 +0900

    [PS] #DS - BigInteger - 뼈대 구성 및 계획
    뼈대 구성 및 계획

--- DataStructure/HW1/code/src/BigInteger.java
+++ DataStructure/HW1/code/src/BigInteger.java
@@ -2,59 +2,68 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-  
-  
+
 public class BigInteger
 {
     public static final String QUIT_COMMAND = "quit";
     public static final String MSG_INVALID_INPUT = "입력이 잘못되었습니다.";
-  
-    // implement this
+
+    // implement this <- 이거 안 써도 됨
     public static final Pattern EXPRESSION_PATTERN = Pattern.compile("");
-  
-  
+
+    int[] num1, num2;
+    char operator;
+
     public BigInteger(int i)
     {
     }
-  
+
     public BigInteger(int[] num1)
     {
     }
-  
+
     public BigInteger(String s)
     {
     }
-  
+
     public BigInteger add(BigInteger big)
     {
     }
-  
+
     public BigInteger subtract(BigInteger big)
     {
     }
-  
+
     public BigInteger multiply(BigInteger big)
     {
     }
-  
+
     @Override
     public String toString()
     {
     }
-  
+
+    private String splitString(String s) {
+        if(Character.isDigit(s.charAt(0))) {
+            s
+        }
+    }
+
     static BigInteger evaluate(String input) throws IllegalArgumentException
     {
         // implement here
         // parse input
-        // using regex is allowed
-  
+        // 공백 제거
+        String noSpaceStr = input.replaceAll(" ","");
+        String firstString = noSpaceStr.
+
         // One possible implementation
         // BigInteger num1 = new BigInteger(arg1);
         // BigInteger num2 = new BigInteger(arg2);
         // BigInteger result = num1.add(num2);
         // return result;
     }
-  
+
     public static void main(String[] args) throws Exception
     {
         try (InputStreamReader isr = new InputStreamReader(System.in))
@@ -65,7 +74,7 @@ public class BigInteger
                 while (!done)
                 {
                     String input = reader.readLine();
-  
+
                     try
                     {
                         done = processInput(input);
@@ -78,11 +87,11 @@ public class BigInteger
             }
         }
     }
-  
+
     static boolean processInput(String input) throws IllegalArgumentException
     {
         boolean quit = isQuitCmd(input);
-  
+
         if (quit)
         {
             return true;
@@ -91,11 +100,11 @@ public class BigInteger
         {
             BigInteger result = evaluate(input);
             System.out.println(result.toString());
-  
+
             return false;
         }
     }
-  
+
     static boolean isQuitCmd(String input)
     {
         return input.equalsIgnoreCase(QUIT_COMMAND);
