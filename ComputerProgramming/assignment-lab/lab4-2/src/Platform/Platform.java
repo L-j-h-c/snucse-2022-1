546e170f395f0d429bee66045371808f1fdbdc1d
L-j-h-c <ckrgkswnsgh@naver.com>
2022-04-06 15:17:23 +0900

    [PS] #CP - Lab4 - Platform Class 구현
    Platform Class 구현

--- ComputerProgramming/assignment-lab/lab4-2/src/Platform/Platform.java
+++ ComputerProgramming/assignment-lab/lab4-2/src/Platform/Platform.java
@@ -1,6 +1,40 @@
 package Platform;
 
+import Platform.Games.Dice;
+import Platform.Games.RockPaperScissors;
+
+import java.util.Scanner;
+
 public class Platform {
-    public float run() { return -0.0f; }
-    public void setRounds(int num) { }
+    private int rounds = 1;
+    private boolean isSetRounds = false;
+
+    public float run() {
+        int sum = 0;
+        int total = rounds;
+        Dice dice = new Dice();
+        RockPaperScissors rsp = new RockPaperScissors();
+
+        Scanner scanner = new Scanner(System.in);
+        int gameType = scanner.nextInt();
+
+        for (int i=1; i<=rounds; i++) {
+            if (gameType == 0) {
+                int status = dice.playGame();
+                if(status == 1) sum += 1;
+            } else {
+                int status = rsp.playGame();
+                if(status == 1) sum += 1;
+            }
+        }
+
+        return (sum / (float)total);
+    }
+
+    public void setRounds(int num) {
+        if (!isSetRounds) {
+            rounds = num;
+            isSetRounds = true;
+        }
+    }
 }
