7a0002f2843976be9324fe6c5c9081403a768b2b
L-j-h-c <ckrgkswnsgh@naver.com>
2022-04-06 14:47:48 +0900

    [PS] #CP - Lab4 - RockScissorPaper 구현
    RockScissorPaper 구현

--- ComputerProgramming/assignment-lab/lab4-2/src/Platform/Games/RockPaperScissors.java
+++ ComputerProgramming/assignment-lab/lab4-2/src/Platform/Games/RockPaperScissors.java
@@ -1,5 +1,38 @@
 package Platform.Games;
+import java.util.Scanner;
 
 public class RockPaperScissors {
-    public int playGame() { return -1; }
+    public int playGame() {
+        Scanner scanner = new Scanner(System.in);
+        String userRCP = scanner.nextLine();
+
+        String opponentRCP = makeOpponentRCP((int) (1+ Math.random() * 3));
+
+        System.out.println(userRCP + " " + opponentRCP);
+
+        if(userRCP == opponentRCP) {
+            return 0;
+        } else if (userRCP == "rock") {
+            if (opponentRCP == "scissor") return 1;
+            else return -1;
+        } else if (userRCP == "scissor") {
+            if (opponentRCP == "paper") return 1;
+            else return -1;
+        } else {
+            if (opponentRCP == "rock") return 1;
+            else return -1;
+        }
+    }
+
+    private String makeOpponentRCP(int opponentCase) {
+        switch (opponentCase) {
+            case 1: return "scissor";
+            case 2: return "rock";
+            case 3: return "paper";
+            default: {
+                System.out.println("error");
+                return "error";
+            }
+        }
+    }
 }
