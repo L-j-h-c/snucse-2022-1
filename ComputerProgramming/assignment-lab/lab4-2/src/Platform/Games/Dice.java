494c0ef3874086dee63080a9564adb69c1a411e0
L-j-h-c <ckrgkswnsgh@naver.com>
2022-04-06 14:27:00 +0900

    [PS] #CP - Lab4 - Dice 클래스 구현
    Dice 클래스 구현

--- ComputerProgramming/assignment-lab/lab4-2/src/Platform/Games/Dice.java
+++ ComputerProgramming/assignment-lab/lab4-2/src/Platform/Games/Dice.java
@@ -1,5 +1,16 @@
 package Platform.Games;
 
 public class Dice {
-    public int playGame() { return -1; }
+    public int playGame() {
+        int userDice = (int) (Math.random() * 100);
+        int opponentDice = (int) (Math.random() * 100);
+
+        int result = Integer.compare(userDice, opponentDice);
+
+        System.out.println(userDice + " " + opponentDice);
+
+        if (result==1) return 1;
+        else if (result==-1) return -1;
+        else return 0;
+    }
 }
