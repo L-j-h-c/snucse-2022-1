6530c4ca987e0d27c37ff37280763c40a28b027a
L-j-h-c <ckrgkswnsgh@naver.com>
2022-04-16 21:54:50 +0900

    [PS] #CP - HW2 - bankaccount 기본 메서드 구현
     bankaccount 기본 메서드 구현

--- ComputerProgramming/HomeWork/Solution/HW2/HW2 /problem1/src/bank/BankAccount.java
+++ ComputerProgramming/HomeWork/Solution/HW2/HW2 /problem1/src/bank/BankAccount.java
@@ -16,6 +16,9 @@ class BankAccount {
 
     BankAccount(String id, String password, int balance) {
         //TODO: Problem 1.1
+        this.id = id;
+        this.password = password;
+        this.balance = balance;
     }
 
     BankAccount(String id, String password, int balance, String question, String answer) {
@@ -25,22 +28,42 @@ class BankAccount {
 
     boolean authenticate(String password) {
         //TODO: Problem 1.1
+        if (this.password.equals(password)) return true;
+        else return false;
     }
 
     void deposit(int amount, int transId) {
         //TODO: Problem 1.1
+        this.balance += amount;
+        this.events[eventIndex++] = new DepositEvent(amount, transId);
     }
 
     boolean withdraw(int amount, int transId) {
         //TODO: Problem 1.1
+        if(this.balance>=amount) {
+            this.balance -= amount;
+            this.events[eventIndex++] = new WithdrawEvent(amount, transId);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     void receive(int amount, int transId) {
         //TODO: Problem 1.1
+        this.balance += amount;
+        this.events[eventIndex++] = new ReceiveEvent(amount, transId);
     }
 
     boolean send(int amount, int transId) {
         //TODO: Problem 1.1
+        if(this.balance>=amount) {
+            this.balance -= amount;
+            this.events[eventIndex++] = new SendEvent(amount, transId);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     boolean secondaryAuthenticate(String questionAnswer) {
