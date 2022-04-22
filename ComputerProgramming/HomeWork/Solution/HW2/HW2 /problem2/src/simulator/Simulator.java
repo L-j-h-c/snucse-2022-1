0b4d01fb028658100e9e00f3229df1611f5ec5b9
L-j-h-c <ckrgkswnsgh@naver.com>
2022-04-22 21:28:45 +0900

    [PS] #CP - HW2 - 완료
    HW2 - 완료

--- ComputerProgramming/HomeWork/Solution/HW2/HW2 /problem2/src/simulator/Simulator.java
+++ ComputerProgramming/HomeWork/Solution/HW2/HW2 /problem2/src/simulator/Simulator.java
@@ -4,40 +4,87 @@ import relay.player.Player;
 import relay.map.Map;
 import java.util.ArrayList;
 
-//public class Simulator {
-//    final static int maxTeamPlayerNum=4;
-//    private final ArrayList<Player> humanPlayers;
-//    private final ArrayList<Player> animalPlayers;
-//    private final Map map;
-//    private boolean raceFinish = false; // For testbench
-//    public String[] raceLogForEval = new String[200]; // For evaluation, don't remove
-//
-//    private Player currentHuman;
-//    private Player currentAnimal;
-//
-//    public Simulator(ArrayList<Player> humanPlayers, ArrayList<Player> animalPlayers, Map map){
-//        //TODO: Problem 2.3
-//	    //for ascending order of position
-//        java.util.Collections.sort(humanPlayers);
-//        java.util.Collections.sort(animalPlayers);
-//    }
-//
-//
-//    public void snapshot() {
-//        //TODO: Problem 2.3
-//    }
-//
-//
-//    public Message talk(int time) {
-//        Message msg = new Message(currentHuman, currentAnimal, map, time);
-//        raceLogForEval[time]=msg.toString();
-//        return msg;
-//    }
-//
-//    public boolean getRaceFinish(){return raceFinish;}
-//
-//
-//     public void simulate() {
-//        //TODO: Problem 2.3
-//    }
-//}
+public class Simulator {
+    final static int maxTeamPlayerNum=4;
+    private final ArrayList<Player> humanPlayers;
+    private final ArrayList<Player> animalPlayers;
+    private final Map map;
+    private boolean raceFinish = false; // For testbench
+    public String[] raceLogForEval = new String[200]; // For evaluation, don't remove
+
+    private Player currentHuman;
+    private Player currentAnimal;
+
+    private int animalIndex = 1;
+    private int humanIndex = 1;
+
+    public Simulator(ArrayList<Player> humanPlayers, ArrayList<Player> animalPlayers, Map map){
+        //TODO: Problem 2.3
+	    //for ascending order of position
+        this.humanPlayers = humanPlayers;
+        this.animalPlayers = animalPlayers;
+        this.map = map;
+
+        java.util.Collections.sort(humanPlayers);
+        java.util.Collections.sort(animalPlayers);
+
+        currentHuman = humanPlayers.get(0);
+        currentAnimal = animalPlayers.get(0);
+        currentHuman.setPlayerNum(1);
+        currentAnimal.setPlayerNum(1);
+        currentHuman.setNextPlayerPosition(humanPlayers.get(1).getPosition());
+        currentAnimal.setNextPlayerPosition(animalPlayers.get(1).getPosition());
+    }
+
+
+    public void snapshot() {
+        //TODO: Problem 2.3
+        if(currentAnimal.getPosition() == animalPlayers.get(animalIndex).getPosition() &&
+        animalPlayers.size() > animalIndex && animalIndex < animalPlayers.size()) {
+            currentAnimal.passBaton(animalPlayers.get(animalIndex));
+            currentAnimal = animalPlayers.get(animalIndex);
+            currentAnimal.setPlayerNum(animalIndex+1);
+            if(animalIndex<animalPlayers.size()-1) currentAnimal.setNextPlayerPosition(animalPlayers.get(animalIndex+1).getPosition());
+            if(animalIndex<animalPlayers.size()-1) animalIndex++;
+        }
+        if(currentHuman.getPosition() == humanPlayers.get(humanIndex).getPosition() &&
+        humanPlayers.size() > humanIndex && humanIndex < humanPlayers.size()) {
+            currentHuman.passBaton(humanPlayers.get(humanIndex));
+            currentHuman = humanPlayers.get(humanIndex);
+            currentHuman.setPlayerNum(humanIndex+1);
+            if(humanIndex<humanPlayers.size()-1) currentHuman.setNextPlayerPosition(humanPlayers.get(humanIndex+1).getPosition());
+            if(humanIndex<humanPlayers.size()-1) humanIndex++;
+        }
+    }
+
+    public Message talk(int time) {
+        Message msg = new Message(currentHuman, currentAnimal, map, time);
+        raceLogForEval[time]=msg.toString();
+        return msg;
+    }
+
+    public boolean getRaceFinish(){return raceFinish;}
+
+     public void simulate() {
+        //TODO: Problem 2.3
+         int time = 0;
+         while(!getRaceFinish()) {
+             currentHuman.hear(talk(time));
+             if(!currentHuman.getThrowUp()) currentHuman.move();
+             if(!currentAnimal.getThrowUp()) currentAnimal.move();
+             snapshot();
+             if(currentAnimal.getPosition()==map.getMapEnd()
+                     || currentHuman.getPosition()==map.getMapEnd()) {
+                 time++;
+                 currentHuman.hear(talk(time));
+                 raceFinish=true;
+             }
+             else if (currentAnimal.getThrowUp() && currentHuman.getThrowUp()) {
+                 time++;
+                 currentHuman.hear(talk(time));
+                 raceFinish=true;
+             }
+             time++;
+         }
+    }
+}
