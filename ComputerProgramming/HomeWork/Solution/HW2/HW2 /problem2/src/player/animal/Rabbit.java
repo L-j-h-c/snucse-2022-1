89de0f374a3719a6e2c39a21bde30d5c7beca61d
L-j-h-c <ckrgkswnsgh@naver.com>
2022-04-22 21:27:37 +0900

    [PS] #CP - HW2 - Runner 및 Rabbit 구현
    Runner 및 Rabbit 구현

--- ComputerProgramming/HomeWork/Solution/HW2/HW2 /problem2/src/player/animal/Rabbit.java
+++ ComputerProgramming/HomeWork/Solution/HW2/HW2 /problem2/src/player/animal/Rabbit.java
@@ -3,29 +3,36 @@ package relay.player.animal;
 import relay.player.Throwable;
 import relay.map.Map;
 
-//public class Rabbit extends Animal implements Throwable {
-//
-//    public Rabbit(Map map){this(0, map);}
-//    public Rabbit(double position, Map map) {
-//        super(position,3,map);
-//    }
-//
-//    @Override
-//    public void move() {
-//        //TODO: Problem 2.2
-//    }
-//
-//    @Override
-//    public String toCustomString() {
-//        //TODO: Problem 2.2
-//    }
-//
-//    public boolean throwUp(double position, Map map){
-//        //TODO: Problem 2.2
-//    }
-//    @Override
-//    public boolean getThrowUp(){
-//        //TODO: Problem 2.2
-//    }
-//
-//}
+public class Rabbit extends Animal implements Throwable {
+
+    public Rabbit(Map map){this(0, map);}
+    public Rabbit(double position, Map map) {
+        super(position,3,map);
+    }
+
+    @Override
+    public void move() {
+        //TODO: Problem 2.2
+        // 물 위에 있냐? 물 위에 있다면 이미 포기하지 않았다는 가정임
+            this.setPosition(this.getPosition()+this.getMovableDistance(this.getVelocity()));
+    }
+
+    @Override
+    public String toCustomString() {
+        //TODO: Problem 2.2
+        return this.getPlayerNumString()+" animal player, rabbit";
+    }
+
+    public boolean throwUp(double position, Map map){
+        //TODO: Problem 2.2
+        if(this.getMap().getOnWater(this.getPosition())) {
+            return true;
+        } else return false;
+    }
+
+    @Override
+    public boolean getThrowUp(){
+        //TODO: Problem 2.2
+        return throwUp(this.getPosition(), this.getMap());
+    }
+}
--- ComputerProgramming/HomeWork/Solution/HW2/HW2 /problem2/src/player/human/Runner.java
+++ ComputerProgramming/HomeWork/Solution/HW2/HW2 /problem2/src/player/human/Runner.java
@@ -4,34 +4,60 @@ import relay.player.Throwable;
 import relay.map.Map;
 import relay.simulator.Message;
 
-//public class Runner extends Human implements Throwable{
-//
-//    public Runner(Map map){this(0, map);}
-//    public Runner(double position, Map map) {
-//        super(position, 2, map);
-//    }
-//
-//    @Override
-//    public void hear(Message message) {
-//        //TODO: Problem 2.2
-//    }
-//
-//    @Override
-//    public void move() {
-//        //TODO: Problem 2.2
-//    }
-//
-//    @Override
-//    public String toCustomString() {
-//        //TODO: Problem 2.2
-//    }
-//
-//    public boolean throwUp(double position, Map map){
-//        //TODO: Problem 2.2
-//    }
-//    @Override
-//    public boolean getThrowUp(){
-//        //TODO: Problem 2.2
-//    }
-//}
+public class Runner extends Human implements Throwable{
+
+    public Runner(Map map){this(0, map);}
+    public Runner(double position, Map map) {
+        super(position, 2, map);
+    }
+
+    @Override
+    public void hear(Message message) {
+        //TODO: Problem 2.2
+        if(!this.getMap().getOnWater(this.getPosition())) {
+            if(message.getDistance()<2) {
+                if(this.getEyesight().canSeeNextPlayer(this.getPosition()) || this.getEyesight().canSeeMapEnd(this.getPosition())) {
+                    this.setVelocity(2.5);
+                } else this.setVelocity(2);
+            } else setVelocity(2);
+        }
+    }
+
+    @Override
+    public void move() {
+        //TODO: Problem 2.2
+
+        // 물 위에 있냐? 물 위에 있다면 이미 포기하지 않았다는 가정임
+        if(this.getMap().getOnWater(this.getPosition())) {
+            this.setPosition(this.getPosition()+this.getMovableDistance(1.0));
+        } // 물 위가 아닌 경우
+        else {
+            this.setPosition(this.getPosition()+this.getMovableDistance(this.getVelocity()));
+        }
+    }
+
+    @Override
+    public String toCustomString() {
+        //TODO: Problem 2.2
+        return this.getPlayerNumString()+" human player, runner";
+    }
+
+    public boolean throwUp(double position, Map map){
+        //TODO: Problem 2.2
+        if(this.getMap().getOnWater(this.getPosition())) {
+            if(this.getEyesight().canSeeNextPlayer(this.getPosition())
+                    || this.getEyesight().canSeeMapEnd(this.getPosition())) {
+                return false;
+            } else {
+                return true;
+            }
+        } else return false;
+    }
+
+    @Override
+    public boolean getThrowUp(){
+        //TODO: Problem 2.2
+        return throwUp(this.getPosition(), this.getMap());
+    }
+}
 
