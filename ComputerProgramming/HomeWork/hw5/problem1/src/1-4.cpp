ec68509292d2eddd261fa363cc07028eecfe337b
L-j-h-c <ckrgkswnsgh@naver.com>
2022-06-04 05:36:59 +0900

    [PS] #CP - HW5 - p1-4

--- ComputerProgramming/HomeWork/hw5/problem1/src/1-4.cpp
+++ ComputerProgramming/HomeWork/hw5/problem1/src/1-4.cpp
@@ -2,7 +2,26 @@
 
 int* PascalTriangle(int N) {
     // TODO: problem 1.4
-
-    return NULL;
+    int* arr = new int[N-1];
+    int* brr = new int[N-1];
+    arr[0]=1;
+    brr[0]=1;
+    for(int i=0; i<N; i++) {
+        for(int j=0; j<=i; j++) {
+            if(j==0) {
+                brr[j]=1;
+                continue;
+            }
+            if(j==i) {
+                brr[j]=1;
+                break;
+            }
+            brr[j] = arr[j-1] + arr[j];
+        }
+        for(int j=0; j<=i; j++) {
+            arr[j] = brr[j];
+        }
+    }
+    return arr;
 }
 
