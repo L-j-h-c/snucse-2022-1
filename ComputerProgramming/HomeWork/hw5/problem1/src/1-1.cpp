64bd6872dccd0237413a5a414af40aa804b33fab
L-j-h-c <ckrgkswnsgh@naver.com>
2022-06-04 03:43:26 +0900

    [PS] #CP - HW5 - P1-1

--- ComputerProgramming/HomeWork/hw5/problem1/src/1-1.cpp
+++ ComputerProgramming/HomeWork/hw5/problem1/src/1-1.cpp
@@ -3,5 +3,11 @@
 bool IsPalindrome(std::string s) {
     // TODO: problem 1.1
 
+    if(s.length()==0 || s.length()==1) return true;
+
+    for(int i =0; i<s.length()-2*i-1; i++) {
+        if(s[i] != s[s.length()-1-i]) return false;
+    }
+
     return true;
 }
