dd1843f6b4ffd5cafc8e8651723724575552b420
L-j-h-c <ckrgkswnsgh@naver.com>
2022-06-04 23:23:54 +0900

    [PS] #CP - HW5 - p2-2

--- ComputerProgramming/HomeWork/Solution/hw5/problem2/src/RunLength.cpp
+++ ComputerProgramming/HomeWork/Solution/hw5/problem2/src/RunLength.cpp
@@ -1,4 +1,5 @@
 #include "RunLength.h"
+#include "vector"
 
 using namespace std;
 RunLength::RunLength() : run_length_unit_(NULL), num_data_(0), data_bit_width_(0), length_bit_width_(0) {}
@@ -30,8 +31,83 @@ std::ostream& operator<<(std::ostream& os, const RunLengthUnit& rlu){
 
 void RunLength::Encode(const char* file_name){
     // TODO: problem 2.2
+    std::ifstream fin;
+    fin.open(file_name);
 
+    string s;
+    vector<struct RunLengthUnit> tempRLU, subTempRLU;
+    int maxLength = pow(2,length_bit_width_);
 
+    int count = 0;
+    int checkRuns = 1;
+    int lastNum = -1;
+    while (true) {
+
+        if(count==0) {
+            getline(fin, s);
+            data_bit_width_ = std::stoi(s);
+            count++;
+            continue;
+        }
+        if(count==1) {
+            getline(fin, s);
+            count++;
+            continue;
+        }
+
+        getline(fin, s);
+        if(fin.eof()) {
+            if(s.empty()) {
+                struct RunLengthUnit r = {lastNum,checkRuns};
+                tempRLU.push_back(r);
+                break;
+            }
+            if(!s.empty()&&std::stoi(s) == lastNum) checkRuns++;
+            struct RunLengthUnit r = {std::stoi(s),checkRuns};
+            tempRLU.push_back(r);
+        } else {
+            if(std::stoi(s) == lastNum) checkRuns++;
+            else {
+                struct RunLengthUnit r = {lastNum,checkRuns};
+                if(lastNum!=-1) tempRLU.push_back(r);
+                checkRuns=1;
+            }
+        }
+
+        lastNum = std::stoi(s);
+        count++;
+    }
+
+    vector<struct RunLengthUnit>::iterator itor = tempRLU.begin();
+
+    count = 0;
+
+    for (; itor != tempRLU.end(); itor++) {
+        if(itor->length>maxLength) {
+            int initData = itor->data;
+            int initLength = itor->length;
+            for( ;initLength>maxLength; initLength-=maxLength) {
+                struct RunLengthUnit r = {initData,maxLength};
+                subTempRLU.push_back(r);
+            }
+            struct RunLengthUnit r = {initData,initLength};
+            subTempRLU.push_back(r);
+            continue;
+        }
+        struct RunLengthUnit r = {itor->data,itor->length};
+        subTempRLU.push_back(r);
+    }
+
+    vector<struct RunLengthUnit>::iterator itor2 = subTempRLU.begin();
+
+    count = 0;
+    run_length_unit_ = new RunLengthUnit[itor2->length];
+    num_data_ = itor2->length;
+    for (; itor2 != subTempRLU.end(); itor2++) {
+        run_length_unit_[count++] = *itor2;
+    }
+
+    fin.close();
 }
 
 double RunLength::Evaluate(const char* file_name) {
