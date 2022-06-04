586c7c37854321504dcd0364e821d8399edd970d
L-j-h-c <ckrgkswnsgh@naver.com>
2022-06-05 00:18:41 +0900

    [PS] #CP -HW5 - p2-4

--- ComputerProgramming/HomeWork/Solution/hw5/problem2/src/Delta.cpp
+++ ComputerProgramming/HomeWork/Solution/hw5/problem2/src/Delta.cpp
@@ -1,4 +1,6 @@
 #include "Delta.h"
+#include "vector"
+#include "math.h"
 
 using namespace std;
 
@@ -29,7 +31,62 @@ std::ostream& operator<<(std::ostream& os, const DeltaUnit& du){
 
 void Delta::Encode(const char* file_name){
     // TODO: problem 2.4
+    std::ifstream fin;
+    fin.open(file_name);
 
+    string s;
+
+    int deltaStandard;
+    vector<int> tmpIntVec;
+
+    int count = 0;
+    int lastNum = 0;
+    while (true) {
+
+        if(count==0) {
+            getline(fin, s);
+            data_bit_width_ = std::stoi(s);
+            count++;
+            continue;
+        }
+        if(count==1) {
+            getline(fin, s);
+            count++;
+            continue;
+        }
+        if(count==2) {
+            getline(fin, s);
+            deltaStandard = std::stoi(s);
+            base_data_=deltaStandard;
+            lastNum = deltaStandard;
+            count++;
+            continue;
+        }
+
+        getline(fin, s);
+        if(fin.eof()) {
+            break;
+        } else {
+            tmpIntVec.push_back(std::stoi(s)-lastNum);
+            lastNum = std::stoi(s);
+        }
+        count++;
+    }
+
+    vector<int>::iterator itor = tmpIntVec.begin();
+
+    count = 0;
+    int tmpBit = 0;
+
+    delta_unit_= new DeltaUnit[tmpIntVec.size()];
+    num_data_ = tmpIntVec.size()+1;
+    for (; itor != tmpIntVec.end(); itor++) {
+        if(abs(*itor)>tmpBit) tmpBit=abs(*itor);
+        delta_unit_[count++].delta = *itor;
+    }
+    delta_bit_width_ = ceil(log2(tmpBit+1))+1;
+
+    fin.close();
 }
 
 double Delta::Evaluate(const char* file_name) {
