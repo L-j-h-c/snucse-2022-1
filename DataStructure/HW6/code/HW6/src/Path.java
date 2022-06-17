b36b9e8a4b9e6a4cd16f6d192f5571190af80ec1
L-j-h-c <ckrgkswnsgh@naver.com>
2022-06-17 18:37:19 +0900

    [PS] #DS - HW6 - Path 클래스

--- DataStructure/HW6/code/HW6/src/Path.java
+++ DataStructure/HW6/code/HW6/src/Path.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Path {
+import java.util.ArrayList;
+
+public class Path {
+    ArrayList<Station> savedPath = new ArrayList<>();
+    Long totalWeight = 0L;
+
+    public Path() {
+
+    }
+
+    public void replacePath(ArrayList<Station> path, Long weight) {
+        this.savedPath = path;
+        totalWeight = weight;
+    }
 }
