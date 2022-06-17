9d575e4fe13c8b4277dab262579a9e138f6f9a87
L-j-h-c <ckrgkswnsgh@naver.com>
2022-06-17 18:37:10 +0900

    [PS] #DS - HW6 - Pair 클래스

---
+++ DataStructure/HW6/code/HW6/src/Pair.java
@@ -0,0 +1,11 @@
+public class Pair {
+   Station begin;
+   Station destination;
+   Long weight;
+
+   public Pair(Station begin, Station destination, Long weight) {
+      this.begin = begin;
+      this.destination = destination;
+      this.weight = weight;
+   }
+}
